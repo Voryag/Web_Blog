4fa986dcc07e88bbfab22db0e4f990882a381337
Voryag <sha4iklein@mail.ru>
2024-02-06 21:39:51 +0400

    I DO REDIRECT TO NEW PAGE WITH BUTTON

--- myblog/polls/urls.py
+++ myblog/polls/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index_home, name="index_home"),
-    path("redirect_create_page/", views.redirect_create_page, name="redirect_create_page"),
-    path("create_notification_page/", views.new_create_page, name="create_notification_page.html")
+    path("", views.render_index, name="render_index"),
+    path("create/", views.redirect_create_page, name="create"),
+    path("delete/", views.redirect_create_page, name="delete"),
 ]
--- myblog/polls/views.py
+++ myblog/polls/views.py
@@ -1,22 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from django.shortcuts import redirect
 
 
-def index_home(request):
-    return render(request, 'index.html')
+def render_index(request):
+    return render(request, "index.html")
 
 def redirect_create_page(request):
-    if request.method == 'POST':
-        print('1')
-        return HttpResponseRedirect("1")
+    if request.method == "POST":
+        print("1")
+        return HttpResponse("1")
 
-        if request.POST.get('create'):
-            print('Create button is touched')
-            return HttpResponseRedirect(reverse('create_notification_page.html'))
-        elif request.POST.get('delete'): # ПЕРЕАДРЕСАЦИЮ НЕ ВЫПОЛНЯТЬ
-            return HttpResponseRedirect(reverse('delete'))
+        #if request.POST.get('create'):
+        #    print('Create button is touched')
+        #    return HttpResponseRedirect(reverse('create_notification_page.html'))
+        #elif request.POST.get('delete'): # ПЕРЕАДРЕСАЦИЮ НЕ ВЫПОЛНЯТЬ
+        #    return HttpResponseRedirect(reverse('delete'))
 
     return render(request, "index.html")
 
