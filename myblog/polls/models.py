dc67aa60a2c1dcf5b1578339f32451d3f2c97782
Voryag <sha4iklein@mail.ru>
2024-02-18 12:11:28 +0400

    create class for DB

--- myblog/polls/models.py
+++ myblog/polls/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+#   notes = Note.objects.all() -- take all notes
+#
+#   note = Note(title='Заголовок', content='Содержимое заметки')
+#   note.save() -- make new note
+#
+#   note = Note.objects.get(id=1)
+#   note.delete() -- delete note
+#
+#
+
+class Note(models.Model):
+    note = models.CharField(max_length=400)
--- myblog/polls/views.py
+++ myblog/polls/views.py
@@ -3,13 +3,18 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from django.shortcuts import redirect
 
+from models import Note
+
 
 def render_index(request):
     return render(request, "index.html")
 
 def redirect_create_page(request):
     note = request.POST.get('note')
-    print(note)
+    if note != None:
+        print(note)
+        note_in_base = Note(note)
+        note.save()
 
     if request.method == "POST":
         if request.POST.get('create'):
